a61af3b5e0e529cb726034e31cb1c73b977068b4
Adrian Waddell <adrian.waddell@roche.com>
2021-12-08 16:33:59 +0100

    address more superlinter issues

--- R/gen_args.R
+++ R/gen_args.R
@@ -1,7 +1,7 @@
 
 #' General Argument Name Convention
 #'
-#' @param adam_db (`dm`) object containing the adam Datasets.
+#' @param adam_db (`dm`) object containing the ADaM datasets
 #' @param armvar (`character`) variable used for column splitting
 #' @param deco (`character`) decoration with `title`, `subtitles` and `main_footer` content
 #' @param lbl_overall (`character`) label used for overall column, if set to `NULL` the overall column is omitted
@@ -11,9 +11,7 @@
 #' @param pmap ('data frame') preprocessing map, see `std_pmap` for the required format
 #' @param group (`list of lists`) for group-dependent data binning
 #'
-#' @details the following arguments are better provided through the study object:
-#'   - lbl_overall
-#'   - armvar
+#' @details the following arguments are better provided through the study object: `lbl_overall`, `armvar`.
 #'
 gen_args <- function(
   adam_db, armvar, lbl_overall, prune_0, deco, .study, group,
