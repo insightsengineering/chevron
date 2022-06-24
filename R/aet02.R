4b3ae7c1eede76c621edbc7c247ba8b9cfd0af23
Meixh2021 <80785063+Meixh2021@users.noreply.github.com>
2022-06-24 10:54:19 -0400

    Update R/aet02.R
    Co-authored-by: b_falquet <64274616+BFalquet@users.noreply.github.com>

--- R/aet02.R
+++ R/aet02.R
@@ -145,8 +145,7 @@ aet02_1_pre <- function(adam_db) {
   adam_db %>%
     dm_zoom_to("adae") %>%
     filter(.data$ANL01FL == "Y") %>%
-    dm_update_zoomed() %>%
-    dm_explicit_na(na_level = "No Coding available")
+    dm_update_zoomed() 
 }
 
 # Version2 ----
