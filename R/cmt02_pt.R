09789c0d8eaff97aa2a92902aa29b842e85bc216
b_falquet <64274616+BFalquet@users.noreply.github.com>
2022-07-11 17:41:13 +0200

    add ... (#214)
    Co-authored-by: benoit <benoit.falquet@roche.com>

--- R/cmt02_pt.R
+++ R/cmt02_pt.R
@@ -91,13 +91,14 @@ cmt02_pt_1_lyt <- function(armvar = .study$planarm,
 #' @describeIn cmt02_pt_1_main `cmt02_pt_1` Preprocessing
 #'
 #' @inheritParams gen_args
+#' @param ... not used.
 #'
 #' @export
 #'
 #' @examples
 #' syn_test_data() %>%
 #'   cmt02_pt_1_pre()
-cmt02_pt_1_pre <- function(adam_db) {
+cmt02_pt_1_pre <- function(adam_db, ...) {
   checkmate::assert_class(adam_db, "dm")
 
   adam_db %>%
