c8c30304305cf0f3cbffbfabca60ee09f8b5f7e2
benoit <benoit.falquet@roche.com>
2022-07-27 08:51:15 +0200

    test

---
+++ tests/testthat/test-run.R
@@ -0,0 +1,8 @@
+# aet04_1 ----
+
+test_that("aet04_1 works as expected", {
+  res <- run(aet04_1, syn_test_data(), prune_0 = TRUE)
+  expect_snapshot(res)
+})
+
+  res <- run(aet04_1, syn_test_data(), prune_0 = FALSE)
