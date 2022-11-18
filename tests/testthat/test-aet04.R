920dc6fbdfcc77e35f55e6af3d68082b04be897c
duanx9 <xiaoli.duan@roche.com>
2022-11-18 08:22:14 +0000

    Sample data review aet04 (#140)

--- tests/testthat/test-aet04.R
+++ tests/testthat/test-aet04.R
@@ -29,14 +29,14 @@ test_that("aet04 can handle all NA values", {
 test_that("aet04 can handle some NA values", {
   new_aebodsys <- c(NA, "", as.character(syn_data$adae$AEBODSYS[-c(1, 2)]))
   new_aedecod <- c(NA, "", as.character(syn_data$adae$AEDECOD[-c(1, 2)]))
-  new_ATOXGR <- c(NA, "", as.character(syn_data$adae$ATOXGR[-c(1, 2)]))
+  new_atoxgr <- c(NA, "", as.character(syn_data$adae$ATOXGR[-c(1, 2)]))
 
   proc_data <- syn_data %>%
     dm_zoom_to("adae") %>%
     mutate(
       AEBODSYS = factor(.env$new_aebodsys),
       AEDECOD = factor(.env$new_aedecod),
-      ATOXGR = factor(.env$new_ATOXGR)
+      ATOXGR = factor(.env$new_atoxgr)
     ) %>%
     dm_update_zoomed()
 
