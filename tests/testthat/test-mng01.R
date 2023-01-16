6735717859a2c6ca4cf8f28e7dffb14dbccf1a8d
b_falquet <64274616+BFalquet@users.noreply.github.com>
2023-01-16 15:30:34 +0100

    358 BUG wrong check in mng01 (#359)
    https://github.com/insightsengineering/chevron/issues/358
    close #358 
    
    fix the check in mng01 so that unnamed color vectors are treated
    accepted.
    
    thank you for the review
    
    Co-authored-by: benoit <benoit.falquet@roche.com>

--- tests/testthat/test-mng01.R
+++ tests/testthat/test-mng01.R
@@ -1,5 +1,16 @@
 test_that("mng01_1 works as expected", {
   pre_data <- mng01_1_pre(syn_data, dataset = "adlb")
-  raw_res <- mng01_1_main(pre_data, dataset = "adlb")
+  raw_res <- mng01_1_main(pre_data, dataset = "adlb", )
   checkmate::expect_list(raw_res, len = 3, types = "ggplot")
+})
+
+test_that("mng01_1 works as expected with custom color set", {
+  col <- c(
+    "A: Drug X" = "black",
+    "B: Placebo" = "blue",
+    "C: Combination" = "gray"
+  )
+
+  res <- run(mng01_1, syn_data, dataset = "adlb", line_col = col)
+  checkmate::expect_list(res, len = 3, types = "ggplot")
 })
