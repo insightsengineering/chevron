c2f95d3cfea83d567faab964f7788c58c4a1bd00
Emily de la Rua <59304861+edelarua@users.noreply.github.com>
2023-01-30 16:37:58 -0500

    Add LBT14 (#369)
    Closes #251
    
    ---------
    
    Signed-off-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    Co-authored-by: b_falquet <64274616+BFalquet@users.noreply.github.com>

--- tests/testthat/test-default_tlg.R
+++ tests/testthat/test-default_tlg.R
@@ -173,6 +173,20 @@ test_that("lbt05_1 functions with default argument value return expected result 
   expect_snapshot(res)
 })
 
+test_that("lbt14_1 functions with default argument value return expected result with test data", {
+  pre_data <- lbt14_1_pre(syn_data)
+  raw_res <- lbt14_1_main(pre_data)
+  res <- lbt14_1_post(raw_res)
+  expect_snapshot(res)
+})
+
+test_that("lbt14_2 functions with default argument value return expected result with test data", {
+  pre_data <- lbt14_2_pre(syn_data)
+  raw_res <- lbt14_2_main(pre_data)
+  res <- lbt14_2_post(raw_res)
+  expect_snapshot(res)
+})
+
 test_that("mht01_1 functions with default argument value return expected result with test data", {
   pre_data <- mht01_1_pre(syn_data)
   raw_res <- mht01_1_main(pre_data)
