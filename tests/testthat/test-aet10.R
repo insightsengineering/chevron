61402107bade4a94b2f0fd0bea43cd248f00d6f5
lvc11 <chenkai.lv@roche.com>
2023-03-03 17:17:33 +0800

    Add aet10 unit test

---
+++ tests/testthat/test-aet10.R
@@ -0,0 +1,28 @@
+test_that("aet10_1 can handle NA values", {
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = factor(NA),
+      AEDECOD = factor(NA)
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet10_1, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet10_1 can handle some NA values", {
+  new_aebodsys <- c(NA, "", as.character(syn_data$adae$AEBODSYS[-c(1, 2)]))
+  new_aedecod <- c(NA, "", as.character(syn_data$adae$AEDECOD[-c(1, 2)]))
+
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = .env$new_aebodsys,
+      AEDECOD = .env$new_aedecod
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet10_1, proc_data))
+  expect_snapshot(res)
+})
