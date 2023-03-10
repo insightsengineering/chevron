c31691ab74192f8bd9b352488cb9c2ab5934b242
Chenkai Lv （吕晨恺） <chenkai.lv@roche.com>
2023-03-10 06:55:14 +0000

    Update report_null function

--- R/utils.R
+++ R/utils.R
@@ -275,10 +275,11 @@ set_decoration <- function(x, deco) {
 #' @return original `TableTree` or a null report if no observation are found in the table.
 #'
 report_null <- function(tlg) {
+  checkmate::assert_true(is.null(tlg) || rtables::is_rtable(tlg))
+
   if (is.null(tlg) || nrow(tlg) == 0L) {
     null_report
   } else {
-    checkmate::assert_true(rtables::is_rtable(tlg))
     tlg
   }
 }
