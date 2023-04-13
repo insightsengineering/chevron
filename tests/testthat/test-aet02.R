8b9418de1fb1062e2d1e4c3980f66a496efd65f0
duanx9 <114642081+duanx9@users.noreply.github.com>
2023-04-13 16:39:20 +0800

    update aet02  (#406)
    close #401 
    
    - [x] Update aet02_1, aet02_2, and aet02_3 (some pieces, e.g. formats,
    are outdated)
    - [x] Add unit tests (including checking NA values, etc.) for each
    variant
    - [x] Refreshed aet02_3 lyt according to TLG template 
    - [x] Check the table counts values + ordering: including comparing
    aet02 table output w/o chevron (using both syn_data and data from
    entimice)
    - [x] Add additional unit test to check aet02 now is able to handle
    correct ordering (when row counts are tied table should be ordered
    alphabetically)

--- tests/testthat/test-aet02.R
+++ tests/testthat/test-aet02.R
@@ -1,24 +1,129 @@
 # aet02 ----
 
-test_that("aet02 fails on incomplete data input", {
-  syn_data <- syn_data %>%
+test_that("aet02_1 fails on incomplete data input", {
+  proc_data <- syn_data %>%
     dm_zoom_to("adae") %>%
     mutate(
       AEDECOD = NULL
     ) %>%
     dm_update_zoomed()
+
   expect_error(
-    run(aet02_1, syn_data),
+    run(aet02_1, proc_data),
     "AEDECOD not in adam_db"
   )
 })
 
-test_that("aet02 handle empty data input", {
-  syn_data <- syn_data %>%
+test_that("aet02_1 handle empty data input", {
+  proc_data <- syn_data %>%
     dm_zoom_to("adae") %>%
     filter(
       AEDECOD == ""
     ) %>%
     dm_update_zoomed()
-  expect_silent(run(aet02_1, syn_data))
+
+  expect_silent(run(aet02_1, proc_data))
+})
+
+test_that("aet02_1 can handle correct ordering", {
+  new_aebodsys <- c("cl YZ.1", "cl YY.1", as.character(syn_data$adae$AEBODSYS[-c(1, 2)]))
+  new_aedecod <- c("dcd YZ.1.1.1.1", "dcd YY.1.1.1.1", as.character(syn_data$adae$AEDECOD[-c(1, 2)]))
+
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = .env$new_aebodsys,
+      AEDECOD = .env$new_aedecod
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_1, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet02_1 can handle NA values", {
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = NA_character_,
+      AEDECOD = NA_character_
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_1, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet02_1 can handle some NA values", {
+  new_aebodsys <- c(NA_character_, "", as.character(syn_data$adae$AEBODSYS[-c(1, 2)]))
+  new_aedecod <- c(NA_character_, "", as.character(syn_data$adae$AEDECOD[-c(1, 2)]))
+
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = .env$new_aebodsys,
+      AEDECOD = .env$new_aedecod
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_1, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet02_2 can handle NA values", {
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = NA_character_,
+      AEDECOD = NA_character_,
+      AEHLT = NA_character_
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_2, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet02_2 can handle some NA values", {
+  new_aebodsys <- c(NA_character_, "", as.character(syn_data$adae$AEBODSYS[-c(1, 2)]))
+  new_aedecod <- c(NA_character_, "", as.character(syn_data$adae$AEDECOD[-c(1, 2)]))
+  new_aehlt <- c(NA_character_, "", as.character(syn_data$adae$AEHLT[-c(1, 2)]))
+
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEBODSYS = .env$new_aebodsys,
+      AEDECOD = .env$new_aedecod,
+      AEHLT = .env$new_aehlt
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_2, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet02_3 can handle NA values", {
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEDECOD = NA_character_
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_3, proc_data))
+  expect_snapshot(res)
+})
+
+test_that("aet02_3 can handle some NA values", {
+  new_aedecod <- c(NA_character_, "", as.character(syn_data$adae$AEDECOD[-c(1, 2)]))
+
+  proc_data <- syn_data %>%
+    dm_zoom_to("adae") %>%
+    mutate(
+      AEDECOD = .env$new_aedecod
+    ) %>%
+    dm_update_zoomed()
+
+  res <- expect_silent(run(aet02_3, proc_data))
+  expect_snapshot(res)
 })
