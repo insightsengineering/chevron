e58dd0e48c8400ac674e7bdea7ebebc13ee55fc3
benoit <benoit.falquet@roche.com>
2023-05-11 09:44:32 +0200

    lbt07

--- tests/testthat/test-default_tlg.R
+++ tests/testthat/test-default_tlg.R
@@ -173,10 +173,3 @@ test_that("lbt05_1 functions with default argument value return expected result 
   res <- lbt05_1_post(raw_res)
   expect_snapshot(res)
 })
-
-test_that("lbt07_1 functions with default argument value return expected result with test data", {
-  pre_data <- lbt07_1_pre(syn_data)
-  raw_res <- lbt07_1_main(pre_data)
-  res <- lbt07_1_post(raw_res)
-  expect_snapshot(res)
-})
