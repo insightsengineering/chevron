7915c98db2bc958ffcba25bb6645a6c86ab84fd5
duanx9 <xiaoli.duan@roche.com>
2023-05-17 08:33:15 +0000

    redesign main (#380)

--- R/fstg01.R
+++ R/fstg01.R
@@ -8,13 +8,19 @@
 #'
 #' @inheritParams gen_args
 #' @param dataset (`string`) the name of a table in the `adam_db` object.
+#' @param arm_var (`string`) the arm variable name used for group splitting.
+#' @param rsp_var (`string`) the responder variable name to flag whether each subject is a responder or not.
+#' @param subgroups (`character`) the subgroups variable name to list baseline risk factors.
+#' @param strata_var (`character`) required if stratified analysis is performed.
 #' @param ... Further arguments passed to `g_forest` and `extract_rsp_subgroups`. For details, see
 #' the documentation in `tern`.
 #' Commonly used arguments include `col_symbol_size`, `col`, `vline`, `groups_lists`, `conf_level`,
 #' `method`, `label_all`, etc.
 #'
 #' @note
-#'  * `adam_db` object must contain the table specified by `dataset` with the columns specified by  `arm_var`.
+#'  * `adam_db` object must contain the table specified by `dataset` with `"PARAMCD"`, `"ARM"`,
+#'  `"AVALC"`, and the columns specified by `"subgroups"` which is denoted as
+#'  `c("SEX", "AGEGR1", "RACE")` by default.
 #'
 #' @return a list of `ggplot` objects.
 #' @export
@@ -28,26 +34,25 @@ fstg01_main <- function(adam_db,
   assert_all_tablenames(adam_db, c("adsl", dataset))
 
   variables <- list(
-    rsp = rsp_var,
     arm = arm_var,
+    rsp = rsp_var,
     subgroups = subgroups,
     strata_var = strata_var
   )
 
-  args <- list(...)
-  control_cox <- execute_with_args(extract_rsp_subgroups, ...)
-
-  df <- extract_rsp_subgroups(
+  df <- execute_with_args(extract_rsp_subgroups,
     variables = variables,
     data = adam_db[[dataset]],
-    conf_level = .9
+    ...
   )
 
   result <- basic_table() %>%
     tabulate_rsp_subgroups(df, vars = c("n_tot", "n", "n_rsp", "prop", "or", "ci"))
 
-  g_forest(
-    tbl = result
+  execute_with_args(
+    g_forest,
+    tbl = result,
+    ...
   )
 }
 
@@ -56,7 +61,7 @@ fstg01_main <- function(adam_db,
 #' @inheritParams fstg01_main
 #'
 #' @export
-fstg01_pre <- function(adam_db, dataset = "adrs", ...) {
+fstg01_pre <- function(adam_db, dataset = "adrs", rsp = c("CR", "PR", "SD", "PD"), ...) {
   adam_db[[dataset]] <- adam_db[[dataset]] %>%
     mutate(is_rsp = .data$AVALC %in% rsp)
 
@@ -89,12 +94,12 @@ fstg01_post <- function(tlg, ...) {
 #' pre_data$adrs <- pre_data$adrs %>% droplevels()
 #'
 #' proc_data <- log_filter(pre_data, PARAMCD == "OVRINV", "adrs")
-#' run(fstg01, proc_data, dataset = "adrs", line_col = col)
+#' run(fstg01, proc_data, rsp = c("CR"), dataset = "adrs", line_col = col)
 #'
 #' proc_data <- log_filter(pre_data, PARAMCD == "BESRSPI", "adrs")
 #' run(fstg01, proc_data,
 #'   rsp = c("CR", "PR"), subgroups = c("SEX", "AGEGR1", "RACE"),
-#'   strata = c("STRATA2"), conf_level = 0.90, dataset = "adrs"
+#'   conf_level = 0.90, dataset = "adrs"
 #' )
 fstg01 <- chevron_g(
   main = fstg01_main,
