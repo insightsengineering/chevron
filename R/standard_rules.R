06ab8d5500a421d2ad336dfa7c5494686acd8314
Liming Li <liming.li@roche.com>
2023-05-18 14:17:31 +0000

    also fix lbt14 issue

--- R/standard_rules.R
+++ R/standard_rules.R
@@ -6,4 +6,24 @@ nocoding <- rule("No Coding Available" = c("", NA))
 missing_rule <- rule("<Missing>" = c("", NA))
 #' Empty rule
 #' @export
-empty <- rule()
+empty_rule <- rule()
+#' Get grade rule
+#' @param direction (`string`) of abnormality direction.
+#' @param missing (`string`) method to deal with missing
+#' @export
+get_grade_rule <- function(direction = "high", missing = "incl") {
+  rule_arg <- list()
+  if (direction == "high") {
+    rule_arg[["Not High"]] <- c("0", "-1", "-2", "-3", "-4")
+    rule_arg[as.character(1:4)] <- as.character(1:4)
+  } else {
+    rule_arg[["Not Low"]] <- c("0", "1", "2", "3", "4")
+    rule_arg[as.character(1:4)] <- as.character(-1:-4)
+  }
+  if (missing == "incl") {
+    rule_arg$Missing <- c(NA, "")
+  } else if (missing == "gr_0") {
+    rule_arg[[1]] <- c(rule_arg[[1]], NA, "")
+  }
+  rule(.lst = rule_arg)
+}
