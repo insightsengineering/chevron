aab0db4fdc0babf44d052424dc5e527e200c7571
duanx9 <xiaoli.duan@roche.com>
2023-06-13 09:00:55 +0000

    refactor aet05 and aet05_all (#379)

--- R/aet05.R
+++ R/aet05.R
@@ -3,6 +3,7 @@
 #' @describeIn aet05 Main TLG function
 #'
 #' @inheritParams gen_args
+#' @param arm_var (`string`) the arm variable used for arm splitting.
 #' @param ... Further arguments passed to `tern::control_incidence_rate()`.
 #'
 #' @details
@@ -12,7 +13,7 @@
 #'  * Keep zero count rows by default.
 #'
 #' @note
-#'  * `adam_db` object must contain an `adaette` table with the columns `""` and `""`.
+#'  * `adam_db` object must contain an `adaette` table with the columns `"PARAMCD"`, `"AVAL"`, and `"CNSR"`.
 #'
 #'
 #' @export
@@ -21,11 +22,12 @@ aet05_main <- function(adam_db,
                        arm_var = "ACTARM",
                        ...) {
   assert_all_tablenames(adam_db, c("adsl", "adaette"))
-  assert_valid_var_pair(adam_db$adsl, adam_db$adaette, arm_var)
-  assert_valid_variable(adam_db$adaette, "USUBJID", empty_ok = TRUE, types = list(c("character", "factor")))
+  checkmate::assert_string(arm_var)
   assert_valid_variable(adam_db$adsl, c("USUBJID", arm_var), types = list(c("character", "factor")))
+  assert_valid_variable(adam_db$adaette, c("USUBJID", arm_var), types = list(c("character", "factor")))
   assert_valid_variable(adam_db$adaette, "AVAL", types = list("numeric"), lower = 0)
-  assert_valid_variable(adam_db$adaette, "n_events", types = list("logical"))
+  assert_valid_variable(adam_db$adaette, "n_events", types = list("numeric"), integerish = TRUE, lower = 0L, upper = 1L)
+  assert_valid_var_pair(adam_db$adsl, adam_db$adaette, arm_var)
   control <- execute_with_args(control_incidence_rate, ...)
 
   lyt <- aet05_lyt(
@@ -42,7 +44,11 @@ aet05_main <- function(adam_db,
 
 #' `aet05` Layout
 #'
-#' @inheritParams aet05_main
+#' @inheritParams gen_args
+#' @param vars (`string`) variable for the primary analysis variable to be iterated over.
+#' @param n_events (`string`) variable to count the number of events observed.
+#' @param control (`list`) parameters for estimation details, specified by using the helper function
+#' control_incidence_rate().
 #'
 #' @keywords internal
 #'
@@ -67,7 +73,7 @@ aet05_lyt <- function(arm_var,
 #'
 aet05_pre <- function(adam_db, ...) {
   adam_db$adaette <- adam_db$adaette %>%
-    filter("TTE" %in% .data$PARAMCD) %>%
+    filter(grepl("TTE", .data$PARAMCD)) %>%
     mutate(PARAMCD = droplevels(.data$PARAMCD)) %>%
     mutate(
       n_events = as.integer(.data$CNSR == 0)
@@ -98,9 +104,9 @@ aet05_post <- function(tlg, prune_0 = FALSE, ...) {
 #' @export
 #'
 #' @examples
-#' run(aet05, syn_data)
+#' run(aet05, syn_data, time_unit_output = 100)
 #'
-#' run(aet05, syn_data, conf_type = "exact", time_unit_output = 100)
+#' run(aet05, syn_data, conf_type = "exact", conf_level = 0.90, time_unit_output = 100)
 aet05 <- chevron_t(
   main = aet05_main,
   preprocess = aet05_pre,
--- R/aet05_all.R
+++ R/aet05_all.R
@@ -3,45 +3,43 @@
 #' @describeIn aet05_all Main TLG function
 #'
 #' @inheritParams gen_args
-#' @param occurrence (`character`) time intervals with respect to first occurrence or all occurrences.
+#' @param arm_var (`string`) the arm variable used for arm splitting.
+#' @param ... Further arguments passed to `tern::control_incidence_rate()`.
 #'
 #' @details
 #'  * Total patient-years at risk is the sum over all patients of the time intervals (in years).
 #'  * Split columns by arm, typically `ACTARM`.
+#'  * The table allows confidence level to be adjusted, default is 2-sided 5%.
 #'  * Keep zero count rows by default.
 #'
 #' @note
-#'  * `adam_db` object must contain an `adaette` table with the columns `""` and `""`.
+#'  * `adam_db` object must contain an `adaette` table with the columns `"PARAMCD"`, `"AVAL"`, and `"CNSR"`.
 #'
 #'
 #' @export
 #'
 aet05_all_main <- function(adam_db,
-                       arm_var = "ACTARM",
-                       ...) {
-  lyt <- aet05_all_lyt(
-    arm_var = arm_var
+                           arm_var = "ACTARM",
+                           ...) {
+  assert_all_tablenames(adam_db, c("adsl", "adaette"))
+  checkmate::assert_string(arm_var)
+  assert_valid_variable(adam_db$adsl, c("USUBJID", arm_var), types = list(c("character", "factor")))
+  assert_valid_variable(adam_db$adaette, c("USUBJID", arm_var), types = list(c("character", "factor")))
+  assert_valid_variable(adam_db$adaette, "AVAL", types = list("numeric"), lower = 0)
+  assert_valid_variable(adam_db$adaette, "n_events", types = list("numeric"), integerish = TRUE, lower = 0L)
+  assert_valid_var_pair(adam_db$adsl, adam_db$adaette, arm_var)
+  control <- execute_with_args(control_incidence_rate, ...)
+
+  lyt <- aet05_lyt(
+    arm_var = arm_var,
+    vars = "AVAL",
+    n_events = "n_events",
+    control = control
   )
 
   tbl <- build_table(lyt, adam_db$adaette, alt_counts_df = adam_db$adsl)
 
   tbl
-}
-
-#' `aet05_all` Layout
-#'
-#' @inheritParams aet05_all_main
-#'
-#' @keywords internal
-#'
-aet05_all_lyt <- function(arm_var) {
-  lyt <- basic_table(show_colcounts = TRUE) %>%
-    split_cols_by(arm_var) %>%
-    estimate_incidence_rate(
-      vars = "AVAL",
-      n_events = "n_events",
-      control = control_incidence_rate(conf_level = 0.95, conf_type = "exact", time_unit_output = 100)
-    )
 }
 
 #' @describeIn aet05_all Preprocessing
@@ -51,27 +49,17 @@ aet05_all_lyt <- function(arm_var) {
 #' @export
 #'
 aet05_all_pre <- function(adam_db, ...) {
-  adam_db$adaette <- adam_db$adaette %>%
-    filter("TOT" %in% .data$PARAMCD | .data$PARAMCD == "AEREPTTE") %>%
-    mutate(PARAMCD = droplevels(.data$PARAMCD)) %>%
-    mutate(n_events = as.integer(n_events),
-    aval = occurrence == "all" & PARAMCD == "AEREPTTE" ~ AVAL) %>%
-    filter(!is.na(.data$aval))
+  anl_tte <- adam_db$adaette %>%
+    filter(.data$PARAMCD == "AEREPTTE")
 
-    adam_db
-}
+  anl_events <- adam_db$adaette %>%
+    filter(grepl("TOT", .data$PARAMCD)) %>%
+    mutate(n_events = as.integer(AVAL)) %>%
+    select(USUBJID, ACTARM, n_events)
 
-#' @describeIn aet05_all Postprocessing
-#'
-#' @inheritParams gen_args
-#'
-#' @export
-#'
-aet05_all_post <- function(tlg, prune_0 = FALSE, ...) {
-  if (prune_0) {
-    tlg <- smart_prune(tlg)
-  }
-  std_postprocess(tlg)
+  adam_db$adaette <- full_join(anl_tte, anl_events, by = c("USUBJID", "ACTARM"))
+
+  adam_db
 }
 
 #' `AET05_ALL` Table 1 (Default) Adverse Event Rate Adjusted for Patient-Years at Risk - All Occurrences.
@@ -83,12 +71,12 @@ aet05_all_post <- function(tlg, prune_0 = FALSE, ...) {
 #' @export
 #'
 #' @examples
-#' run(aet05_all, syn_data)
+#' run(aet05_all, syn_data, time_unit_output = 100)
 #'
-#' run(aet05_all, syn_data, conf_type = "exact")
+#' run(aet05, syn_data, conf_type = "exact", conf_level = 0.90, time_unit_output = 100)
 aet05_all <- chevron_t(
   main = aet05_all_main,
   preprocess = aet05_all_pre,
-  postprocess = aet05_all_post,
+  postprocess = aet05_post,
   adam_datasets = c("adsl", "adaette")
 )
