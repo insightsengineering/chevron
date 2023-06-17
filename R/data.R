a7c197d334afb173693a92b8fbd5c321ea906b28
Liming Li <liming.li@roche.com>
2023-06-17 16:30:29 +0000

    update ctc and mla reference data

--- R/data.R
+++ R/data.R
@@ -24,3 +24,22 @@
 #'     - `advs`
 #' @source based on `scda::synthetic_cdisc_data("rcd_2022_06_27")`
 "syn_data"
+
+
+# ctc_dir ----
+
+#' ctc grade data
+"ctc_dir"
+
+# mla_grid ----
+#' mla grade data
+"mla_grid"
+
+#' Get ctc grade data by version
+#' @param version (`number`) version of ctc grading.
+#' @export
+get_ctc <- function(version = 4) {
+  ctc_dir %>%
+    filter(CTCv == version) %>%
+    select(PARAMCD, GRADE_DIR)
+}
