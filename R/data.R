47a450c1b39309c06ae0c138a60cdc9167a7c6d7
Liming Li <liming.li@roche.com>
2023-06-19 01:47:49 +0000

    separate ctc data

--- R/data.R
+++ R/data.R
@@ -28,18 +28,13 @@
 
 # ctc_dir ----
 
-#' CTC Grade Direction Data
-"ctc_dir"
+#' CTC version 4 Grade Direction Data
+"ctcv4_dir"
+
+#' CTC version 5 Grade Direction Data
+"ctcv5_dir"
 
 # mla_grid ----
 #' MLA Grade Direction Data
 "mla_dir"
 
-#' Get ctc grade data by version
-#' @param version (`number`) version of ctc grading.
-#' @export
-get_ctc <- function(version = 4) {
-  ctc_dir %>%
-    filter(CTCv == version) %>%
-    select(PARAMCD, GRADE_DIR)
-}
