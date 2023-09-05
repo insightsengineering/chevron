bac82dc82cf0ba63c17cce2cc01760c0feab7ef3
benoit <benoit.falquet@roche.com>
2023-09-05 13:24:03 +0200

    add prototype

---
+++ R/rmpt06.R
@@ -0,0 +1,190 @@
+# rmpt06 ----
+
+#' @describeIn rmpt06 Main TLG function
+#'
+#' @inheritParams gen_args
+#' @param method (`character`) the method used to construct the confidence interval.
+#' See [`tern::estimate_proportions`].
+#' @param conf_level (`proportion`) the confidence level of the interval. See [`tern::estimate_proportions`].
+#' @param grade_groups (`list`) the grade groups to be displayed.
+#'
+#' @export
+rmpt06_main <- function(adam_db,
+                        arm_var = "ACTARM",
+                        lbl_overall = NULL,
+                        method = "clopper-pearson",
+                        conf_level = 0.95,
+                        grade_groups = NULL,
+                        ...) {
+  assert_all_tablenames(adam_db, "adsl", "adae")
+  assert_string(arm_var)
+  assert_string(lbl_overall, null.ok = TRUE)
+  assert_choice(
+    method,
+    c(
+      "waldcc", "wald", "clopper-pearson", "wilson", "wilsonc",
+      "strat_wilson", "strat_wilsonc", "agresti-coull", "jeffreys"
+    )
+  )
+  assert_numeric(conf_level, lower = 0, upper = 1)
+  assert_list(grade_groups, null.ok = TRUE)
+
+  assert_valid_variable(adam_db$adsl, "AEFL", type = list("logical"))
+  assert_valid_variable(adam_db$adae, "ATOXGR", na_ok = TRUE, types = list("factor"))
+  assert_valid_variable(adam_db$adae, "AESERFL", types = list("logical"))
+  assert_valid_variable(adam_db$adae, "AEOUT", na_ok = TRUE, types = list("factor"))
+  assert_valid_variable(adam_db$adsl, c("USUBJID", arm_var))
+  assert_valid_variable(adam_db$adae, c(arm_var), types = list(c("character", "factor")))
+  assert_valid_variable(adam_db$adae, "USUBJID", empty_ok = TRUE, types = list(c("character", "factor")))
+  assert_valid_var_pair(adam_db$adsl, adam_db$adae, arm_var)
+
+  lbl_overall <- render_safe(lbl_overall)
+
+  if (is.null(grade_groups)) {
+    grade_groups <- list(
+      "Grade 1" = "1",
+      "Grade 2" = "2",
+      "Grade 3" = "3",
+      "Grade 4" = "4",
+      "Grade 5 (fatal outcome)" = "5"
+    )
+  }
+
+
+  lyt <- rmpt06_lyt(
+    arm_var = arm_var,
+    lbl_overall = lbl_overall,
+    method = method,
+    conf_level = conf_level,
+    grade_groups = grade_groups
+  )
+
+  tbl_adsl <- build_table(lyt$adsl, adam_db$adsl)
+  tbl_adae <- build_table(lyt$adae, adam_db$adae, alt_counts_df = adam_db$adsl)
+
+  rbind(
+    tbl_adsl,
+    tbl_adae
+  )
+}
+
+#' `rmpt06` Layout
+#'
+#' @inheritParams gen_args
+#'
+#' @keywords internal
+#'
+rmpt06_lyt <- function(arm_var,
+                       lbl_overall,
+                       method,
+                       conf_level,
+                       grade_groups) {
+  lyt <- basic_table(show_colcounts = TRUE) %>%
+    split_cols_by_with_overall(arm_var, lbl_overall)
+
+  lyt_adsl <- lyt %>%
+    estimate_proportion(
+      vars = "AEFL",
+      method = method,
+      conf_level,
+      .labels = c(
+        n_prop = render_safe("Number of {patient_label} with at least one adverse event"),
+        prop_ci = paste0(
+          100 * conf_level,
+          render_safe("% CI for % of {patient_label} with at least one AE ("),
+          stringr::str_to_title(method),
+          ")"
+        )
+      )
+    )
+
+  lyt_adae <- lyt %>%
+    analyze_num_patients(
+      vars = "USUBJID",
+      .stats = c("nonunique"),
+      .labels = c(
+        nonunique = "Total number of AEs"
+      ),
+      .formats = list(nonunique = "xx"),
+      show_labels = "hidden"
+    ) %>%
+    count_occurrences_by_grade(
+      var = "ATOXGR",
+      var_labels = render_safe("Total number of {patient_label} with at least one AE by worst grade"),
+      show_labels = "visible",
+      grade_groups = grade_groups
+    ) %>%
+    count_patients_with_flags(
+      "USUBJID",
+      flag_variables = "AESERFL",
+      flag_labels = render_safe("Number of {patient_label} with at least one serious AE"),
+      denom = "N_col"
+    ) %>%
+    count_occurrences(
+      "AEOUT",
+      denom = "n",
+      var_labels = render_safe("Number of {patient_label} with at least one AE by outcome"),
+      show_labels = "visible",
+      drop = FALSE
+    )
+
+
+  list(
+    adsl = lyt_adsl,
+    adae = lyt_adae
+  )
+}
+
+#' @describeIn rmpt06 Preprocessing
+#'
+#' @inheritParams rmpt06_main
+#'
+#' @export
+rmpt06_pre <- function(adam_db, ...) {
+  aeout_rule <- rule(
+    "Fatal outcome" = "FATAL",
+    "Unresolved" = "NOT RECOVERED/NOT RESOLVED",
+    "Recovered/Resolved" = "RECOVERED/RESOLVED",
+    "Resolved with sequelae" = "RECOVERED/RESOLVED WITH SEQUELAE",
+    "Recovering/Resolving" = "RECOVERING/RESOLVING",
+    "Unknown outcome" = "UNKNOWN"
+  )
+
+  adam_db$adsl <- adam_db$adsl %>%
+    mutate(AEFL = .data$USUBJID %in% .env$adam_db$adae$USUBJID)
+
+  adam_db$adae <- adam_db$adae %>%
+    filter(.data$ANL01FL == "Y") %>%
+    mutate(
+      AEOUT = reformat(.data$AEOUT, aeout_rule),
+      AESERFL = if_else(.data$AESER == "Y", TRUE, FALSE)
+    )
+
+  adam_db
+}
+
+#' @describeIn rmpt06 Postprocessing
+#'
+#' @inheritParams gen_args
+#'
+#' @export
+#'
+rmpt06_post <- function(tlg, prune_0 = FALSE, ...) {
+  if (prune_0) {
+    tlg <- smart_prune(tlg)
+  }
+  std_postprocess(tlg)
+}
+
+#' `RMPT06` Table 1 (Default) Seriousness, Outcomes, Severity, Frequency with 95% CI for Risk Management Plan.
+#'
+#' @include chevron_tlg-S4class.R
+#' @export
+#'
+#' @examples
+#' run(rmpt06, syn_data)
+rmpt06 <- chevron_t(
+  main = rmpt06_main,
+  preprocess = rmpt06_pre,
+  postprocess = rmpt06_post
+)
---
+++ tests/testthat/test-rmpt06.R
@@ -0,0 +1,25 @@
+# rmpt06 ----
+
+test_that("rmpt06 works as expected", {
+  res <- rmpt06_pre(syn_data) %>%
+    rmpt06_main() %>%
+    rmpt01_post()
+
+  expect_snapshot(cat(export_as_txt(res, lpp = 100, cpp = 200)))
+})
+
+
+test_that("rmpt06 can handle all NA values", {
+  proc_data <- syn_data
+  proc_data$adae <- proc_data$adae %>%
+    mutate(
+      ATOXGR = factor(NA, levels = 1:5),
+      AEOUT = factor(NA),
+      AESER = NA
+    )
+
+  res <- expect_silent(run(rmpt06, proc_data))
+  expect_snapshot(cat(export_as_txt(res, lpp = 100, cpp = 200)))
+  res <- expect_silent(run(rmpt06, proc_data, prune_0 = TRUE))
+  expect_snapshot(cat(export_as_txt(res, lpp = 100, cpp = 200)))
+})
