4129d6e53004917e848f7dd83e4a63e4bb62b4f3
benoit <benoit.falquet@roche.com>
2023-09-11 15:16:18 +0200

    increase test coverage

--- tests/testthat/test-cml02a_gl.R
+++ tests/testthat/test-cml02a_gl.R
@@ -4,3 +4,14 @@ test_that("cml02a_gl works with admh dataset", {
   expect_message(res <- run(cml02a_gl, syn_data))
   expect_snapshot(cat(export_as_txt(res, lpp = 100)))
 })
+
+# cml02a_gl functions ----
+
+test_that("cml02a_gl works with admh dataset", {
+  expect_message(
+    res <- cml02a_gl_pre(syn_data) %>%
+      cml02a_gl_main() %>%
+      cml02a_gl_post()
+  )
+  expect_snapshot(cat(export_as_txt(res, lpp = 100)))
+})
