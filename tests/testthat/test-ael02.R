997dfb1aa1468cd3294ea8ca6203b85ea6d0d876
benoit <benoit.falquet@roche.com>
2023-09-11 15:24:00 +0200

    increase coverage

--- tests/testthat/test-ael02.R
+++ tests/testthat/test-ael02.R
@@ -19,3 +19,14 @@ test_that("ael02 can handle all missing values", {
   res <- expect_silent(run(ael02, proc_data))
   expect_snapshot(cat(export_as_txt(res[1:100, ], lpp = 100)))
 })
+
+# ael02 functions ----
+
+test_that("ael02 functions work as expected", {
+  res <- expect_silent(
+    ael02_pre(syn_data) %>%
+      ael02_main() %>%
+      ael02_post()
+  )
+  expect_snapshot(cat(export_as_txt(res[1:100, ], lpp = 100)))
+})
