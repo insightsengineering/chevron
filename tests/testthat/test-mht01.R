462da638bf5057d2c95c9a16f01465c6c767280e
b_falquet <64274616+BFalquet@users.noreply.github.com>
2024-05-17 08:30:31 +0200

    746 post processing should follow row_split_var in main function (#749)
    https://github.com/insightsengineering/chevron/issues/746
    
    close #746 
    
    thank you for the review

--- tests/testthat/test-mht01.R
+++ tests/testthat/test-mht01.R
@@ -20,3 +20,9 @@ test_that("mht01 works on NULL in lbl_mhbodsys or lbl_mhdecod", {
   res <- run(mht01, proc_data, lbl_mhbodsys = NULL, lbl_mhdecod = NULL)
   expect_snapshot(cat(export_as_txt(res, lpp = 100)))
 })
+
+test_that("mht01 works with other row_split_var", {
+  proc_data <- syn_data
+  res <- run(mht01, proc_data, row_split_var = c("SEX", "ETHNIC"))
+  expect_snapshot(cat(export_as_txt(res, lpp = 100)))
+})
