a8d87fffa768ec18c2bca8d8110358561a8f5d8d
b_falquet <64274616+BFalquet@users.noreply.github.com>
2025-06-18 13:05:28 +0200

    update tests (#842)
    remove `is_ggplot` as not all integration test environment have access
    to the latest version of ggplot.
    
    thank you for the review

--- tests/testthat/test-fstg01.R
+++ tests/testthat/test-fstg01.R
@@ -4,7 +4,7 @@ test_that("fstg01 works as expected", {
   proc_data <- dunlin::log_filter(syn_data, PARAMCD == "BESRSPI" & ARM %in% c("A: Drug X", "B: Placebo"), "adrs")
   pre_data <- expect_silent(fstg01_pre(proc_data, dataset = "adrs", response = c("CR", "PR")))
   raw_res <- expect_silent(fstg01_main(pre_data, dataset = "adrs"))
-  expect_true(ggplot2::is_ggplot(raw_res))
+  expect_class(raw_res, "gg")
 })
 
 # fstg01 ----
@@ -14,10 +14,10 @@ test_that("fstg01 works as expected with custom color set", {
   proc_data <- dunlin::log_filter(syn_data, PARAMCD == "BESRSPI" & ARM %in% c("A: Drug X", "B: Placebo"), "adrs")
 
   res1 <- expect_silent(run(fstg01, proc_data, response = c("CR", "PR"), dataset = "adrs", col = "gray"))
-  expect_true(ggplot2::is_ggplot(res1))
+  expect_class(res1, "gg")
 
   res2 <- expect_silent(run(fstg01, proc_data, response = c("CR", "PR"), dataset = "adrs", col_symbol_size = NULL))
-  expect_true(ggplot2::is_ggplot(res2))
+  expect_class(res2, "gg")
 })
 
 test_that("fstg01 works if changes are in subgroups, conf_level, and label_all", {
@@ -25,13 +25,13 @@ test_that("fstg01 works if changes are in subgroups, conf_level, and label_all",
   proc_data <- dunlin::log_filter(syn_data, PARAMCD == "BESRSPI" & ARM %in% c("A: Drug X", "B: Placebo"), "adrs")
 
   res1 <- expect_silent(run(fstg01, proc_data, response = c("CR", "PR"), subgroups = NULL, dataset = "adrs"))
-  expect_true(ggplot2::is_ggplot(res1))
+  expect_class(res1, "gg")
 
   res2 <- expect_silent(run(fstg01, proc_data,
     response = c("CR", "PR"), subgroups = c("SEX"),
     conf_level = 0.9, label_all = "All Patients", dataset = "adrs"
   ))
-  expect_true(ggplot2::is_ggplot(res2))
+  expect_class(res2, "gg")
 })
 
 test_that("fstg01 can handle some NA values in subgroups", {
@@ -42,5 +42,5 @@ test_that("fstg01 can handle some NA values in subgroups", {
   proc_data$adrs[5:6, "RACE"] <- NA
 
   res <- expect_silent(run(fstg01, proc_data, response = c("CR", "PR"), dataset = "adrs"))
-  expect_true(ggplot2::is_ggplot(res))
+  expect_class(res, "gg")
 })
