70f84d933e1c0b0bcb79cbe21d1c478ec860c502
b_falquet <bfalquet@protonmail.com>
2025-06-18 12:13:43 +0200

    update tests

--- tests/testthat/test-fstg02.R
+++ tests/testthat/test-fstg02.R
@@ -4,7 +4,7 @@ test_that("fstg02 works as expected", {
   proc_data <- dunlin::log_filter(syn_data, PARAMCD == "OS" & ARM %in% c("A: Drug X", "B: Placebo"), "adtte")
   pre_data <- expect_silent(fstg02_pre(proc_data, dataset = "adtte"))
   raw_res <- expect_silent(fstg02_main(pre_data, dataset = "adtte"))
-  checkmate::assert_true(ggplot2::is_ggplot(raw_res))
+  expect_class(raw_res, "gg")
 })
 
 # fstg02 ----
@@ -14,10 +14,10 @@ test_that("fstg02 works as expected with custom color set", {
   proc_data <- dunlin::log_filter(syn_data, PARAMCD == "OS" & ARM %in% c("A: Drug X", "B: Placebo"), "adtte")
 
   res1 <- expect_silent(run(fstg02, proc_data, dataset = "adtte", col = "gray"))
-  checkmate::assert_true(ggplot2::is_ggplot(res1))
+  expect_class(res1, "gg")
 
   res2 <- expect_silent(run(fstg02, proc_data, dataset = "adtte", col_symbol_size = NULL))
-  checkmate::assert_true(ggplot2::is_ggplot(res2))
+  expect_class(res2, "gg")
 })
 
 test_that("fstg02 works if changes are in subgroups, strata_var, conf_level, and label_all", {
@@ -25,13 +25,13 @@ test_that("fstg02 works if changes are in subgroups, strata_var, conf_level, and
   proc_data <- dunlin::log_filter(syn_data, PARAMCD == "OS" & ARM %in% c("A: Drug X", "B: Placebo"), "adtte")
 
   res1 <- expect_silent(run(fstg02, proc_data, subgroups = NULL, dataset = "adtte"))
-  checkmate::assert_true(ggplot2::is_ggplot(res1))
+  expect_class(res1, "gg")
 
   res2 <- expect_silent(run(fstg02, proc_data,
     subgroups = c("SEX"), strata_var = c("AGEGR1"),
     conf_level = 0.9, label_all = "All Patients", dataset = "adtte"
   ))
-  checkmate::assert_true(ggplot2::is_ggplot(res2))
+  expect_class(res2, "gg")
 })
 
 test_that("fstg02 can handle some NA values in subgroups", {
@@ -42,7 +42,7 @@ test_that("fstg02 can handle some NA values in subgroups", {
   proc_data$adtte[5:6, "RACE"] <- NA
 
   res <- expect_silent(run(fstg02, proc_data, dataset = "adtte"))
-  checkmate::assert_true(ggplot2::is_ggplot(res))
+  expect_class(res, "gg")
 })
 
 test_that("fstg02 can handle customized time units", {
@@ -57,9 +57,9 @@ test_that("fstg02 can handle customized time units", {
     )
 
   res1 <- expect_silent(run(fstg02, proc_data, dataset = "adtte"))
-  checkmate::assert_true(ggplot2::is_ggplot(res1))
+  expect_class(res1, "gg")
 
   proc_data$adtte$AVALU <- "DAYS"
   expect_warning(res2 <- run(fstg02, proc_data, dataset = "adtte"))
-  checkmate::assert_true(ggplot2::is_ggplot(res2))
+  expect_class(res2, "gg")
 })
